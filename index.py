f4be36b014eea6f33d0d3be3ffb4a6801626efc5
infxnnite <140268569+infxnnite8@users.noreply.github.com>
2023-10-10 12:49:39 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+print ("Hello, Welcome to INFXNX Coffeee Shop")
+
+name = input ("What's your name?")
+
+print ("Hello " + name + " , How May I help you today")
+
+
+print ("Would you like to look at our menu?")
+menu = ("Black Coffee , Punkin Spice Lattee , Or Cold Brew")
+
+
+input ("Ok. " + name + " Would you like to take a look at our menu? ")
+
+print ("Yes Of Course. " + menu )
+order = ("Cold Brew")
+
+print ("yes I Would Like A. " + order )
+
+print ("yes certainly " + order + " my pleasure ")
+
+
+
